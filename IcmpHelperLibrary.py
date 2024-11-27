8db13458295174e28371eeb9e40876e5c4547950
Julian Canepa <julian.l.canepa@gmail.com>
2024-11-26 18:55:51 -0700

    Add validation for echo packet.

--- IcmpHelperLibrary.py
+++ IcmpHelperLibrary.py
@@ -154,8 +154,46 @@ class IcmpHelperLibrary:
             self.__packHeader()                 # Header is rebuilt to include new checksum value
 
         def __validateIcmpReplyPacketWithOriginalPingData(self, icmpReplyPacket):
+            """
+            TODO:
+            1) Confirm the following items received are the same as what was sent:
+                sequence number
+                packet identifier
+                raw data
+            2) Set the valid data variable in the IcmpPacket_EchoReply class based on the outcome of the data comparison.
+            3) Create variables within the IcmpPacket_EchoReply class that identify whether each value that can be obtained from the class is valid. For example, the IcmpPacket_EchoReply class has an IcmpIdentifier. Create a variable, such as IcmpIdentifier_isValid, along with a getter function, such as getIcmpIdentifier_isValid(), and a setting function, such as setIcmpIdentifier_isValid(), so you can easily track and identify which data points within the echo reply are valid. Note: There are similar examples within the current skeleton code.
+            4) Create debug messages that show the expected and the actual values along with the comparison result.
+            """
             # Hint: Work through comparing each value and identify if this is a valid response.
-            icmpReplyPacket.setIsValidResponse(True)
+
+            # confirm sequence number, packet identifier and raw data match what was sent
+            is_sequence_match = (icmpReplyPacket.getIcmpSequenceNumber() == self.__packetSequenceNumber)
+
+            if is_sequence_match:
+                print("sequence matches")
+            else:
+                print("sequence mismatch:")
+                print("expected:", self.__packetSequenceNumber)
+                print("actual:", icmpReplyPacket.getIcmpSequenceNumber())
+
+            is_identifier_match = (icmpReplyPacket.getIcmpIdentifier() == self.__packetIdentifier)
+
+            if is_identifier_match:
+                print("packet identifier matches")
+            else:
+                print("packet identifier mismatch:")
+                print("expected:", self.__packetIdentifier)
+                print("actual:", icmpReplyPacket.getIcmpIdentifier())
+
+            is_data_match = (icmpReplyPacket.getIcmpData() == self.__dataRaw)
+            if is_data_match:
+                print("packet data matches")
+            else:
+                print("packet data mismatch:")
+                print("expected:", self.__dataRaw)
+                print("actual:", icmpReplyPacket.getIcmpData())
+
+            icmpReplyPacket.setIsValidResponse(is_sequence_match and is_identifier_match and is_data_match)
             pass
 
         """
@@ -189,8 +227,11 @@ class IcmpHelperLibrary:
                 howLongInSelect = (endSelect - startedSelect)
                 if whatReady[0] == []:  # Timeout
                     print("  *        *        *        *        *    Request timed out.")
-                recvPacket, addr = mySocket.recvfrom(1024)  # recvPacket - bytes object representing data received
+
+                # recvPacket - bytes object representing data received
+                # https://docs.python.org/3/library/socket.html#socket.socket.recvfrom
                 # addr  - address of socket sending data
+                recvPacket, addr = mySocket.recvfrom(1024)
                 timeReceived = time.time()
                 timeLeft = timeLeft - howLongInSelect
                 if timeLeft <= 0:
@@ -200,7 +241,10 @@ class IcmpHelperLibrary:
                     # Fetch the ICMP type and code from the received packet
                     icmpType, icmpCode = recvPacket[20:22]
 
-                    if icmpType == 11:                          # Time Exceeded
+                    if icmpType == 11:
+                        """
+                        time (TTL) exceeded
+                        """
                         print("  TTL=%d    RTT=%.0f ms    Type=%d    Code=%d    %s" %
                                 (
                                     self.getTtl(),
@@ -211,7 +255,10 @@ class IcmpHelperLibrary:
                                 )
                               )
 
-                    elif icmpType == 3:                         # Destination Unreachable
+                    elif icmpType == 3:
+                        """
+                        destination unreachable
+                        """
                         print("  TTL=%d    RTT=%.0f ms    Type=%d    Code=%d    %s" %
                                   (
                                       self.getTtl(),
@@ -222,11 +269,15 @@ class IcmpHelperLibrary:
                                   )
                               )
 
-                    elif icmpType == 0:                         # Echo Reply
+                    elif icmpType == 0:
+                        """
+                        echo reply (destination reached)
+                        """
                         icmpReplyPacket = IcmpHelperLibrary.IcmpPacket_EchoReply(recvPacket)
                         self.__validateIcmpReplyPacketWithOriginalPingData(icmpReplyPacket)
+                        # TODO if is valid, set it...
                         icmpReplyPacket.printResultToConsole(self.getTtl(), timeReceived, addr)
-                        return      # Echo reply is the end and therefore should return
+                        return      # echo reply is the end and therefore should return
 
                     else:
                         print("error")
@@ -333,6 +384,13 @@ class IcmpHelperLibrary:
         Public functions
         """
         def printResultToConsole(self, ttl, timeReceived, addr):
+            """
+            TODO:
+            1) Identify if the echo response is valid and report the error information details. For example, if the raw data is different, print to the console what the expected value and the actual value.
+            2) Currently, the program calculates the round-trip time for each packet and prints it out individually. Modify the code to correspond to the way the standard ping program works. You will need to report the minimum, maximum, and average RTTs at the end of all pings from the client. In addition, calculate the packet loss rate (in percentage). It is recommended to create an output that is easily readable with the amount of data used for a trace route since a ping is the foundation for such functionality.
+            3) Your program can only detect timeouts and destination unreachable when receiving ICMP echo responses. Modify the Pinger program to parse the ICMP response error codes (type 0, 3, 11) and display the corresponding error results to the user in a human-readable form. Examples of ICMP response error codes are 0: Destination Network Unreachable, 1: Destination Host Unreachable. Reference: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+            4) Note: Although this requirement does not explicitly list which ICMP types and codes to implement, nor does it give a specific number to implement, it is easy to differentiate those that are currently valid and related from those that are not. It is recommended to use some kind of data structure to hold the types and codes so you can do a lookup instead of expanding the if/else structure currently in place.
+            """
             bytes = struct.calcsize("d")
             timeSent = struct.unpack("d", self.__recvPacket[28:28 + bytes])[0]
             print("  TTL=%d    RTT=%.0f ms    Type=%d    Code=%d        Identifier=%d    Sequence Number=%d    %s" %
@@ -374,6 +432,10 @@ class IcmpHelperLibrary:
             # we should be confirming values are correct, such as identifier and sequence number and data
 
     def __sendIcmpTraceRoute(self, host):
+        """
+        TODO:
+        Handle sending ICMP echo requests with increasing TTL values and processing the responses to trace the route to the destination.
+        """
         print("sendIcmpTraceRoute Started...") if self.__DEBUG_IcmpHelperLibrary else 0
         # Build code for trace route here
 
@@ -385,6 +447,10 @@ class IcmpHelperLibrary:
         self.__sendIcmpEchoRequest(targetHost)
 
     def traceRoute(self, targetHost):
+        """
+        TODO:
+        Use this function and update the __sendIcmpTraceRoute() function to handle sending ICMP echo requests with increasing TTL values and processing the responses to trace the route to the destination.
+        """
         print("traceRoute Started...") if self.__DEBUG_IcmpHelperLibrary else 0
         self.__sendIcmpTraceRoute(targetHost)
 
